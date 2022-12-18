d5cf64b6419d8d5bdd2f5f7ee859d1d5fb3c79a4
shahrukh010 <khanshahrukhbsc@gmail.com>
2022-12-18 15:22:29 +0530

    working on list

---
+++ LinkedList.java
@@ -0,0 +1,45 @@
+package com.code.main.list;
+
+public class LinkedList{
+
+    class Node{
+
+        private int data;//
+        private Node next;
+
+        public Node(int data){
+            this.data = data;
+        }
+
+    }
+
+    private Node first = null;
+    private Node last  = null;
+
+    public void add(int value){
+
+        Node newNode = new Node(value);
+
+        if(this.first == null){
+            this.first = newNode;
+            this.last  = newNode;
+        }
+        else{
+
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+    }
+
+    public void printNode(){
+
+        Node current = this.first;
+        while(current !=null){
+
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+
+}
---
+++ MainList.java
@@ -0,0 +1,19 @@
+import com.code.main.list.LinkedList;
+
+public class MainList{
+
+
+    public static void main(String...strings){
+
+    LinkedList list = new LinkedList();
+            
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        list.printNode();
+
+    }
+}
