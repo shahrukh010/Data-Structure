f3c9879fca47bb3e3fa09053721023a31e39cd2c
shahrukh010 <khanshahrukhbsc@gmail.com>
2023-01-18 07:44:09 +0530

    optimize trailing zeor

--- January.java
+++ January.java
@@ -26,6 +26,44 @@ class Math{
         }
         return n==res;
     }
+
+    public int factorial(int n){
+
+        int res = 1;
+        while( n !=0 && n !=1){
+
+            res *= n;
+            n--;
+       }
+        return res;
+    }
+
+
+
+        
+        
+
+            public int trailingZero(int n){
+
+                int res = 0;
+                /**
+                 * this logi is not working for high no of factorial like 20,it given 19 digit
+                 * and 19 digit can not hold it data type or same like 251
+                 */
+                /*
+                int fact = factorial(n);
+                while(fact % 10 ==0){
+                    res++;
+                    fact = fact / 10;
+                }
+                */
+
+                for(int i = 5; i <=n; i=i*5){
+                    res +=  n/i;
+                }
+                return res;
+            }
+
 }
         
 public class January{
@@ -40,7 +78,17 @@ public class January{
         //boolean res = math.palindrom(78987);
         //boolean res = math.palindrom(121);
         //boolean res = math.palindrom(8);
-        boolean res = math.palindrom(21);
+       // boolean res = math.palindrom(21);
+        //System.out.println(res);
+//        int res = math.factorial(5);
+ //       int res = math.factorial(6);
+//        int res = math.factorial(0);
+ //       System.out.println(res);
+
+//        int res = math.trailingZero(5);
+       // int res = math.trailingZero(10);
+//        int res = math.trailingZero(24);
+        int res = math.trailingZero(251);
         System.out.println(res);
     }
 
