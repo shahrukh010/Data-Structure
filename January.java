1544812755427507317ca8f2de2e3c819f27969c
shahrukh010 <khanshahrukhbsc@gmail.com>
2023-01-18 07:14:35 +0530

    palindrom

--- January.java
+++ January.java
@@ -12,6 +12,20 @@ class Math{
         }
         return res;
     }
+
+
+    public boolean palindrom(int n){
+
+        int res = 0;
+        int tmp = n;
+
+        while(tmp !=0){
+
+            res = res * 10 + tmp % 10;
+            tmp = tmp/10;
+        }
+        return n==res;
+    }
 }
         
 public class January{
@@ -20,7 +34,13 @@ public class January{
         Math math = new Math();
         //int res = math.countDigit(9235);
         //int res = math.countDigit(38);
-        int res = math.countDigit(3);
+//        int res = math.countDigit(3);
+ //       System.out.println(res);
+
+        //boolean res = math.palindrom(78987);
+        //boolean res = math.palindrom(121);
+        //boolean res = math.palindrom(8);
+        boolean res = math.palindrom(21);
         System.out.println(res);
     }
 
