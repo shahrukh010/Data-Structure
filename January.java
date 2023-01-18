fd298913dae6d325e51fdb5aa3466d2b8cf359c6
shahrukh010 <khanshahrukhbsc@gmail.com>
2023-01-18 21:19:27 +0530

    revise sorting

--- January.java
+++ January.java
@@ -1,4 +1,49 @@
 
+class Sorting{
+
+
+    private void swap(int[]input,int i, int j){
+
+        int tmp = input[i];
+        input[i] = input[j];
+        input[j] = tmp;
+    }
+
+    private int partitionByLomuto(int[]input,int low,int high){
+
+        int i = low-1;
+        int pivot = input[high];
+
+        for(int j = low; j <input.length; j++){
+
+            if(input[j]<pivot){
+
+                i++;
+                swap(input,i,j);
+            }
+        }
+        swap(input,i+1,high);
+        return i+1;
+    }
+
+
+        public int kthSmallestElement(int[]input,int low ,int high,int k){
+
+            while(low<=high){
+
+                int pivot = partitionByLomuto(input,low,high);
+
+                if(pivot == k-1)
+                        return input[pivot];
+
+                else if(pivot > k-1)
+                    high = pivot-1;
+                else
+                    low = pivot+1;
+            }
+            return -1;
+        }
+}
 class Math{
 
 
@@ -86,12 +131,26 @@ class Math{
                     res++;
             }
             }
+
+                                        
+
+                        public boolean isPrime(int n){
+
+                            for(int i = 2 ; i*i<n ;i++){
+                                
+                                if(n%i==0)return false;
+                            }
+                            return true;
+
+                        }
 }
         
 public class January{
 
     public static void main(String...strings){
         Math math = new Math();
+
+        Sorting sort = new Sorting();
         //int res = math.countDigit(9235);
         //int res = math.countDigit(38);
 //        int res = math.countDigit(3);
@@ -119,8 +178,20 @@ public class January{
 
         //int res = math.lcm(4,6);
         //int res = math.lcm(12,15);
-        int res = math.lcm(2,8);
-        System.out.println(res);
+//        int res = math.lcm(2,8);
+ //       System.out.println(res);
+
+//        boolean res = math.isPrime(13);
+ //       System.out.println(res);
+
+
+        //int[] input = {10,4,5,8,11,6,26};
+        //int[] input = {10,5,30,12};
+        int[] input = {30,20,5,10,8};
+        int result = sort.kthSmallestElement(input,0,input.length-1,4);
+        System.out.println(result);
+
+
     }
 
 }
