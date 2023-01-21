3cfb9ca542676dc5c705c6a1541ffc297f5c141b
shahrukh010 <khanshahrukhbsc@gmail.com>
2023-01-22 00:07:31 +0530

    boundary traversal

--- Matrix.java
+++ Matrix.java
@@ -36,4 +36,36 @@ public class Matrix{
                     System.out.print(input[i][j]+",");
         }
     }
+
+
+
+    public void boundaryTraversal(int[][]input){
+
+        int r = input.length;
+        int c = input[0].length;
+
+        if(input.length==1)
+            for(int i = 0; i < input.length; i++)
+                System.out.println(input[0][i]+",");
+
+
+        else if(input[0].length==1)
+            for(int i = 0; i < input.length;i++)
+                System.out.println(input[i][0]+",");
+
+        else{
+
+            for(int index = 0; index <c;index++)
+                System.out.print(input[0][index]+",");
+
+            for(int index = 1; index < r; index++)
+                System.out.print(input[index][c-1]+",");
+
+            for(int index = c-2; index>=1;index--)
+                System.out.print(input[r-1][index]+",");
+
+            for(int index = r-2; index>=1;index--)
+                System.out.print(input[index][0]+",");
+        }
+    }
 }
--- MatrixMain.java
+++ MatrixMain.java
@@ -8,8 +8,10 @@ public class MatrixMain {
 //        matrix.jaggedMatrix();
 
         int[][] pattern = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        matrix.sneakPattern(pattern);
-        System.out.println();
+     //   matrix.sneakPattern(pattern);
+    //    System.out.println();
+        int[][] input = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        matrix.boundaryTraversal(input);
     }
 
 }
