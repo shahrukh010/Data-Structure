7401f4b969c1f5afb8842becff8370a954ae4740
shahrukh010 <khanshahrukhbsc@gmail.com>
2023-01-30 08:18:34 +0530

    structure of hashtable

--- HashMain.java
+++ HashMain.java
@@ -1,9 +1,19 @@
+import com.code.hashtable.Hashtable;
 
 public class HashMain{
 
 
     public static void main(String...strings){
 
+        Hashtable table = new Hashtable(10);
+        table.add(10);
+        table.add(20);
+        table.add(30);
+        table.add(40);
+        table.add(15);
+        table.add(23);
+        table.add(21);
+        System.out.println(table);
 
     }
 
--- Hashtable.java
+++ Hashtable.java
@@ -1,5 +1,37 @@
 package com.code.hashtable;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Hashtable {
 
+    private List<LinkedList<Integer>> table;
+    int bucket = 0;
+
+    public Hashtable(int size){
+        this.bucket = size;
+
+       table = new ArrayList<>();
+
+       for(int index = 0; index < size; index++)
+           table.add(new LinkedList<Integer>());
+    }
+
+    public void add(int data){
+
+        int index = data % this.bucket;
+        table.get(index).add(data);
+    }
+
+    List res = null;
+
+    @Override
+    public String toString(){
+
+        res = new ArrayList<>();
+        for(List list : table){
+            res.add(list);
+        }
+        return res.toString();
+    }
 }
